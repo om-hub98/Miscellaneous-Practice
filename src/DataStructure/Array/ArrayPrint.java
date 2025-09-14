1df5c13c11e111a83cf39770598ea50103bd7e92
omrajpradhan@gmail.com <omrajpradhan@gmail.com>
2025-09-14 23:23:51 +0530

    feat: added array test

---
+++ src/DataStructure/Array/ArrayPrint.java
@@ -0,0 +1,10 @@
+package DataStructure.Array;
+
+import java.util.Arrays;
+
+public class ArrayPrint {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        System.out.println(Arrays.toString(arr));
+    }
+}
